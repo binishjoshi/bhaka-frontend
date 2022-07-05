47073d3ae4eb44bc01f3fb1550946f21fa2a7825
Neo <cyclonejet1@pm.me>
2022-07-05 17:22:23 +0545

    basic styling for player header

--- src/persistent-components/PlayerHeader.jsx
+++ src/persistent-components/PlayerHeader.jsx
@@ -1,18 +1,42 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';  
 import { AuthContext } from '../context/auth-context';
+
+import './player-header.css';
 
 const PlayerHeader = () => {
   const auth = useContext(AuthContext);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const searchChangeHandler = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
   const signOutHandler = () => {
     auth.logout();
   };
 
   return (
-    <>
-      Player Header
-      <button onClick={signOutHandler}>Sign Out</button>
-    </>
+    <div className='player-header-container'>
+      <div className='header-logo'>Bhaka</div>
+      <div className='player-header-searchbox'>
+        <form>
+          <div className='searchbox-container'>
+            <input
+              id='search'
+              type='text'
+              placeholder='Search'
+              onChange={searchChangeHandler}
+              value={searchQuery}
+            />
+          </div>
+        </form>
+      </div>
+      <div className='header-links'>
+        <Link to='/player'>Home</Link>
+        <Link to='/' onClick={signOutHandler}>Sign Out</Link>
+      </div>
+    </div>
   );
 };
 
