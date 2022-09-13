eeb8368a36728b1c50b0f0097160c37b99f946f2
Neo <cyclonejet1@pm.me>
2022-09-13 18:28:08 +0545

    class -> className in svg components

--- src/svg/NextButtonSVG.jsx
+++ src/svg/NextButtonSVG.jsx
@@ -2,7 +2,7 @@ const NextButtonSVG = (props) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      class='icon icon-tabler icon-tabler-player-track-next'
+      className='icon icon-tabler icon-tabler-player-track-next'
       width='24'
       height='24'
       viewBox='0 0 24 24'
--- src/svg/PreviousButtonSVG.jsx
+++ src/svg/PreviousButtonSVG.jsx
@@ -2,7 +2,7 @@ const PreviousButtonSVG = (props) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      class='icon icon-tabler icon-tabler-player-track-prev'
+      className='icon icon-tabler icon-tabler-player-track-prev'
       width='24'
       height='24'
       viewBox='0 0 24 24'
