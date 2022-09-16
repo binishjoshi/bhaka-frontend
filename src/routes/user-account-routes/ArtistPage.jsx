176bb4163bd2544dc78e5012826414f6f1f4a376
Neo <cyclonejet1@pm.me>
2022-09-16 09:35:33 +0545

    artist page

--- src/routes/user-account-routes/ArtistPage.jsx
+++ src/routes/user-account-routes/ArtistPage.jsx
@@ -1,5 +1,58 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { lanAddress } from '../../.lanAddress';
+
+import { useHttpClient } from '../../hooks/http-hook';
+import AlbumsList from '../../ui-elements/AlbumsList';
+
+import './artist-page.css';
+
 const ArtistPage = () => {
-  return <div className='artist-page-container'>artist</div>;
+  let { artistId } = useParams();
+  const [artistInfo, setArtistInfo] = useState(false);
+  const [artistAlbums, setArtistAlbums] = useState(false);
+  //eslint-disable-next-line
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  useEffect(() => {
+    const fetchArtistInfo = async (artistId) => {
+      try {
+        const responseData = await sendRequest(
+          `http://${lanAddress}:5000/api/artists/${artistId}`
+        );
+        setArtistInfo(responseData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const fetchArtistAlbums = async (artistId) => {
+      try {
+        const responseData = await sendRequest(
+          `http://${lanAddress}:5000/api/albums/artist/${artistId}`
+        );
+        setArtistAlbums(responseData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchArtistInfo(artistId);
+    fetchArtistAlbums(artistId);
+  }, [artistId, sendRequest]);
+  return (
+    <div className='artist-page-container'>
+      {artistInfo && (
+        <div className='artist-info-container'>
+          <img alt='artist' src={`http://${lanAddress}:5000/${artistInfo.picture}`} />
+          <div className='artist-text-info'>
+            <span className='artist-title'>{artistInfo.name}</span>
+            <span>{artistInfo.followers}</span>
+            <span className='artist-description'>{artistInfo.description}</span>
+          </div>
+        </div>
+      )}
+      {artistAlbums && <AlbumsList albumsList={artistAlbums} />}
+    </div>
+  );
 };
 
 export default ArtistPage;
---
+++ src/ui-elements/AlbumsList.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+import './albums-list.css';
+
+const AlbumsList = ({ albumsList }) => {
+  return (
+    <table className='albums-list-table'>
+      <thead className='album-table-heading'>
+        <tr>
+          <th scope='col'>#</th>
+          <th scope='col'>Album</th>
+          <th scope='col'>Type</th>
+          <th scope='col'>Interval</th>
+        </tr>
+      </thead>
+      <tbody>
+        {albumsList.map((album) => (
+          <tr className='album-row' key={album.id}>
+            <td>{albumsList.indexOf(album) + 1}</td>
+            <td>
+              <Link className='album-title' to={`/album/${album.id}`}>
+                {album.title}
+              </Link>
+            </td>
+            <td>{album.type}</td>
+            <td>{album.interval}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default AlbumsList;
